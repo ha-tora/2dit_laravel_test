61f958c69766d3c4de28afaf26be38ca29cbfe41
ha_tora <j.2374.04@gmail.com>
2025-05-26 14:28:17 +0600

    models

--- database/migrations/2025_05_26_075356_create_orders_table.php
+++ database/migrations/2025_05_26_075356_create_orders_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->text('comment')->nullable();
         });
     }
 
